84accc15a50ac7fd59f235a1c52edac0cd526363
Muskan Manhas <muskanmanhas9055@gmail.com>
2022-09-27 13:49:10 +0530

    Update Code

--- Projects/src/DropDownList.java
+++ Projects/src/DropDownList.java
@@ -1,13 +1,17 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class AB_testControl {
+public class DropDownList {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\p\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        WebElement DROPDOWN= driver.findElement(By.id("dropdown"));
 
-        driver.get("https://the-internet.herokuapp.com/abtest");
-
+        Select D1=new Select(DROPDOWN);
+        D1.selectByIndex(1);
     }
 }
