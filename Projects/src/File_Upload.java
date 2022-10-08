59c56e1cb109a1dd4ed2532c53caef48acd16410
Muskan Manhas <muskanmanhas9055@gmail.com>
2022-10-08 10:19:43 +0530

    Update Code

--- Projects/src/File_Upload.java
+++ Projects/src/File_Upload.java
@@ -1,12 +1,8 @@
-import org.checkerframework.checker.units.qual.K;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.awt.*;
-import java.awt.datatransfer.StringSelection;
-import java.awt.event.KeyEvent;
 
 public class File_Upload {
 
@@ -16,7 +12,6 @@ public class File_Upload {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/upload");
-
 
         WebElement chooseFile = driver.findElement(By.cssSelector("#file-upload"));
         chooseFile.sendKeys("C:\\Users\\p\\Desktop\\upload\\image\\images.jfif");
